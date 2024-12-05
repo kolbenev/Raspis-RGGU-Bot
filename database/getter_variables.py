4089fb4e935df6f201d203188b574648f9d0aa1e
Dmitry Kolbenev <kolbenev.d.s@gmail.com>
2024-12-05 19:42:00 +0300

    Добавлены модели, инициализирован и настроен алембик, поднята бд через докер.

---
+++ database/getter_variables.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+
+from os import getenv
+
+load_dotenv()
+
+POSTGRES_USER = getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = getenv("POSTGRES_DB")
+HOSTNAME = getenv("HOSTNAME")
